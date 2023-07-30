30fb6261c4d9965b6a1c19e711e0415a0571ec96
SimaSKumbhar <121923172+SimaSKumbhar@users.noreply.github.com>
2023-07-30 18:43:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin:["https://peaceful-alpaca-14fad0.netlify.app"],
+  origin:["https://storied-pavlova-b2b96a.netlify.app/"],
   methods:["GET","POST"],
   credentials:true,
 }));
